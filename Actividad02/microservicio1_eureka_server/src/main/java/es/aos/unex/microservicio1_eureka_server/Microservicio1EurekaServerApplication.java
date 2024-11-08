d51a02f67af1a45fb38200e9c7eb424d37cd8d93
DANIEL <danisanchezp2@gmail.com>
2024-11-08 16:40:41 +0100

    Eureka Client Server OK

--- Actividad02/microservicio1_eureka_server/src/main/java/es/aos/unex/microservicio1_eureka_server/Microservicio1EurekaServerApplication.java
+++ Actividad02/microservicio1_eureka_server/src/main/java/es/aos/unex/microservicio1_eureka_server/Microservicio1EurekaServerApplication.java
@@ -2,7 +2,9 @@ package es.aos.unex.microservicio1_eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class Microservicio1EurekaServerApplication {
 
